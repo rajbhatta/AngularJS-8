4aec8b133f367bf1f3740e94b6d8adfe7f37c620
rajbhatta <bhattaraj21@gmail.com>
2019-12-24 16:56:28 -0700

    Binding form to Model using [(ngModel)]- Part 2

--- 17_Binding form to model - Form handling part2/form-handling-part2/src/app/app.component.ts
+++ 17_Binding form to model - Form handling part2/form-handling-part2/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/User'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user: User;
+
+  constructor() {
+    this.user = new User();
+  }
   title = 'form-handling-part2';
 }

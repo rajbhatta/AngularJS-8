4518b7f8e249cc15f920376009b2c74bfc4c7afc
rajbhatta <bhattaraj21@gmail.cOm>
2019-12-24 01:22:03 -0700

    integrating Angular 8 with bootstrap

---
+++ 15_bootstrap_integration/bootstrap-angular/README.md
@@ -0,0 +1,18 @@
+Major steps to integrate Bootstrap to Angular 8 Project
+First, navigate inside your project’s root folder:
+$ cd bootstrap-angular
+
+
+Next, install Bootstrap 4 and jQuery from npm:
+$ npm install --save bootstrap jquery
+
+modify angular.json as given below:
+
+"styles": [
+      "src/styles.css", 
+        "node_modules/bootstrap/dist/css/bootstrap.min.css"
+      ],
+      "scripts": [
+        "node_modules/jquery/dist/jquery.min.js",
+        "node_modules/bootstrap/dist/js/bootstrap.min.js"
+      ]
---
+++ 15_bootstrap_integration/bootstrap-angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
